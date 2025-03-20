f913e215a66e93701f1a56d5dab72448520401a7
Sahil Kulkarni <sahilkulkarni16@gmail.com>
2025-03-19 22:54:12 -0400

    Update tiv_call.jl

--- tiv_sir_models/tiv_call.jl
+++ tiv_sir_models/tiv_call.jl
@@ -1,7 +1,7 @@
 function tiv_call(conc_c3b_sites)
     tend = 10*60 #seconds
     tspan = (0.0, tend)
-    y0 = zeros(4)
+    y0 = zeros(3)
     y0[1] = conc_c3b_sites*1e6
     y0[2] = 1e-5 
     prob = ODEProblem(TIVfunc!, y0, tspan)
