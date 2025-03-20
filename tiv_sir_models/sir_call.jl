a96aeed8bd48b0dfd9caf829053eee83268d0e60
Sahil Kulkarni <sahilkulkarni16@gmail.com>
2025-03-19 22:53:48 -0400

    Update sir_call.jl

--- tiv_sir_models/sir_call.jl
+++ tiv_sir_models/sir_call.jl
@@ -1,7 +1,7 @@
 function sir_call(conc_c3b_sites)
     tend = 10*60 #seconds
     tspan = (0.0, tend)
-    y0 = zeros(3)
+    y0 = zeros(2)
     y0[1] = conc_c3b_sites*1e6
     y0[2] = 1e-4
     prob = ODEProblem(SIRfunc!, y0, tspan)
